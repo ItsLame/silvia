6b4bd6ed834248d4e6c2ec7d9dcd7a4a92a7001d
Silvia <silvia@firstcallqa.com>
2025-02-16 01:30:04 +0800

    init project setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import {dirname} from "path";
+import {fileURLToPath} from "url";
+import {FlatCompat} from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +11,21 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "indent": ["error", 2],
+      "eol-last": ["error", "always"],
+      "no-unused-vars": ["error", {
+        "argsIgnorePattern": "^_",
+      }],
+      "no-trailing-spaces": "error",
+      "no-multiple-empty-lines": "error",
+      "no-irregular-whitespace": "error",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "error",
+      "semi": ["error", "always"],
+    }
+  }
 ];
 
 export default eslintConfig;
