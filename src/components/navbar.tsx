80ad68fed75e34e38242d1ed2f7fb2fa016fd29f
ItsLame <37344470+ItsLame@users.noreply.github.com>
2025-02-18 23:55:58 +0700

    feat: cool button component

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -17,6 +17,16 @@ const NavbarButton = ({ children, link = "", newTab = false } : {children?: Reac
   );
 };
 
+const DarkModeToggle = () => {
+  return (
+    <button className="rounded-full bg-red-800">
+      <div className="px-4 py-2 rounded-full -translate-y-1 bg-red-200 text-red-600 font-semibold active:translate-y-0 transition-all active:shadow-inner active:shadow-red-800">
+        toggle
+      </div>
+    </button>
+  );
+};
+
 const Navbar = () => {
   const [isBurgerOpened, setIsBurgerOpened] = useState(false);
 
@@ -35,6 +45,7 @@ const Navbar = () => {
         <NavbarButton link="/projects">projects</NavbarButton>
         <NavbarButton link="/contact">contact</NavbarButton>
         <NavbarButton link="https://1drv.ms/b/s!AhwQNlQ3dXFki9I_beUieojh-1Tk6w?e=OQ50iI" newTab>resume</NavbarButton>
+        <DarkModeToggle />
       </div>
     </nav>
   );
