9d0f7f67e85bca6efe2b72cbb80058428d04e01b
ItsLame <37344470+ItsLame@users.noreply.github.com>
2025-03-22 02:56:00 +0700

    feat: update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,22 +9,10 @@ const Home = () => {
       </header>
 
       <main>
-        <p>something on hello...</p>
-        <p>something on hello...</p>
-        <p>something on hello...</p>
-        <p>something on hello...</p>
-        <p>something on hello...</p>
-        <p>something on hello...</p>
-        <p>something on hello...</p>
-        <p>something on hello...</p>
-        <p>something on hello...</p>
-        <p>something on hello...</p>
-        <p>something on hello...</p>
-        <p>something on hello...</p>
-        <p>something on hello...</p>
-        <p>something on hello...</p>
-        <p>something on hello...</p>
-        <p>something on hello...</p>
+        <div className="hero flex flex-col flex-1 min-h-[calc(100vh-11.25rem)] justify-center">
+          <h1 className="text-8xl">hello i&apos;m <span className="name block">silvia</span></h1>
+          <p className="text-xl">a full stack software engineer who loves to build nice looking things.</p>
+        </div>
       </main>
 
       <footer>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -18,10 +18,16 @@ const NavbarButton = ({ children, className = "", link = "", newTab = false } : 
 };
 
 export const DarkModeToggle = () => {
+  const toggleTheme = () => {
+    document.documentElement.classList.toggle("dark",);
+  };
+
   return (
-    <button className="rounded-full bg-red-800">
+    <button className="rounded-full bg-red-800" onClick={toggleTheme}>
       <div className="px-4 py-2 rounded-full -translate-y-1 bg-red-200 text-red-600 font-semibold active:translate-y-0 transition-all active:shadow-inner active:shadow-red-800">
-        toggle
+        {/*<IconDeviceDesktop />*/}
+        {/*<IconSun />*/}
+        {/*<IconMoon />*/}
       </div>
     </button>
   );
