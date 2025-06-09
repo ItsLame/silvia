19f991b3d64d7a39aed8cd2f0c1ed777b6b0a996
ItsLame <37344470+ItsLame@users.noreply.github.com>
2025-06-09 11:05:43 +1000

    fix: "remove metadata order" migration script

--- src/migrations/20250608_123947_remove_metadata_order.ts
+++ src/migrations/20250608_123947_remove_metadata_order.ts
@@ -3,23 +3,23 @@ import { MigrateDownArgs, MigrateUpArgs, } from "@payloadcms/db-mongodb";
 export async function up({ payload, req, session }: MigrateUpArgs): Promise<void> {
   await payload.db.collections["projects"].updateMany(
     {},
-    { $unset: { metadata: { order: null } } }
+    { $unset: { "metadata.order": null } }
   );
 
   await payload.db.collections["blogs"].updateMany(
     {},
-    { $unset: { metadata: { order: null } } }
+    { $unset: { "metadata.order": null } }
   );
 }
 
 export async function down({ payload, req, session }: MigrateDownArgs): Promise<void> {
   await payload.db.collections["projects"].updateMany(
     {},
-    { $set: { metadata: { order: null } } }
+    { $set: { "metadata.order": null } }
   );
 
   await payload.db.collections["blogs"].updateMany(
     {},
-    { $set: { metadata: { order: null } } }
+    { $set: { "metadata.order": null } }
   );
 }
