d2a514a218bca10de099cf15d8ffde9e22997e1f
ItsLame <37344470+ItsLame@users.noreply.github.com>
2025-08-17 16:21:34 +1000

    feat: dark mode toggle colors

--- src/components/DarkModeToggle.tsx
+++ src/components/DarkModeToggle.tsx
@@ -9,8 +9,8 @@ const DarkModeToggle = () => {
   const toggleTheme = useSetAtom(themeAtom);
 
   return (
-    <button className="rounded-full bg-red-800" onClick={toggleTheme}>
-      <div className="px-4 py-2 rounded-full -translate-y-1 bg-red-200 text-red-600 font-semibold active:translate-y-0 transition-all active:shadow-inner active:shadow-red-800">
+    <button className="rounded-full bg-red-800 dark:bg-red-950" onClick={toggleTheme}>
+      <div className="p-2 rounded-full -translate-y-1 bg-red-200 dark:bg-red-900 text-red-600 dark:text-red-300 font-semibold active:translate-y-0 transition-all active:shadow-inner active:shadow-red-800 dark:active:shadow-black">
         {themeStatus.isDark && <IconMoon />}
         {themeStatus.isSystem && <IconDeviceDesktop />}
         {themeStatus.isLight && <IconSun />}
